39fb2d6d9062ca3c6bbde6ca8c910a5bbe2ef401
Stan Hatko <stan_hatko@live.com>
2025-08-31 22:45:31 -0400

    undo last since very slow

--- run_benchmark.py
+++ run_benchmark.py
@@ -4,8 +4,6 @@ Check speed of running LLM on CPU with huggingface transformers.
 """
 
 import os
-import psutil
-import sys
 import time
 
 import torch
@@ -55,45 +53,24 @@ def encode_inputs(tokenizer, inputs: list):
     return x
 
 
-def generate_llm(model, input_data: list, num_cpu: int, max_tokens: int):
+def generate_llm(model, input_data: list, max_tokens: int):
     """
     Run the LLM on the input data.
-    Generate in parallel on specified number of CPUs.
     """
 
-    r = []
-    pids = []
     t1 = time.time()
-
-    for i, x in enumerate(input_data):
-        print("Running for entry:", i)
-
-        p = os.fork()
-        if p == 0:
-            y = [
-                model.generate(
-                    **x,
-                    max_new_tokens=max_tokens,
-                    do_sample=False,
-                )
-                for x in input_data
-            ]
-            print(f"Done child process for {i}.")
-            sys.exit(0)
-        else:
-            print("Continue run for next...")
-            pids.append(p)
-
-    print("Wait for processes to finish...")
-    for p in pids:
-        print("Check for pid:", p)
-        while psutil.pid_exists(p):
-            time.sleep(0.1)
-    print("Done waiting for all processes to finish.")
-
+    outputs = [
+        model.generate(
+            **x,
+            max_new_tokens=max_tokens,
+            do_sample=False,
+        )
+        for x in input_data
+    ]
     t2 = time.time()
-    print(f"Took {round(t2 - t1, 2)} seconds to generate LLM predictions.")
-    breakpoint()
+
+    print(f"Took {round(t2 - t1, 2)} seconds to run LLM.")
+    return outputs
 
 
 def decode_outputs(tokenizer, input_data: list, output_data: list):
@@ -122,7 +99,7 @@ def benchmark_llm(model_name: str, num_queries: int, num_cpu: int, max_tokens: i
     print("Number of queries:", num_queries)
     print("Number of CPU cores to use:", num_cpu)
 
-    torch.set_num_threads(1)
+    torch.set_num_threads(num_cpu)
     tokenizer, model = load_tokenizer_model(model_name)
 
     # TODO: replace hardcoded input!
@@ -134,6 +111,6 @@ def benchmark_llm(model_name: str, num_queries: int, num_cpu: int, max_tokens: i
         ],
     )
 
-    num_outputs = generate_llm(model, inputs, num_cpu, max_tokens)
+    num_outputs = generate_llm(model, inputs, max_tokens)
     final_outputs = decode_outputs(tokenizer, inputs, num_outputs)
     print("Contents of final outputs:", final_outputs)
